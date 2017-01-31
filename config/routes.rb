22611d1266c83dac5e0cfa0a3155649873ff49bf
Bruno G <brunogaleski@gmail.com>
2017-01-31 23:24:56 +0000

    List and search movies
    Signed-off-by: Bruno G <brunogaleski@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+	resources :movies do
+        collection do
+                get 'search'
+        end
+	end
+
+  get 'store/index'
+
+  root 'store#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
